3e9002ee08d1f59920f83c54b3df32c4f71d255d
Pedro Gasparini <136598710+pedrogasparini@users.noreply.github.com>
2025-07-08 18:18:39 -0300

    Delete GoViajes directory

--- GoViajes/src/assets/icons/icons.js
+++
@@ -1,7 +0,0 @@
-export { FaBell as NotificationIcon } from "react-icons/fa";       // Notification
-export { FaSearch as SearchIcon } from "react-icons/fa";           
-export { FaHome as DashboardIcon } from "react-icons/fa";              
-export { FaBoxOpen as ProductsIcon } from "react-icons/fa";        
-export { FaCog as SettingsIcon } from "react-icons/fa";            
-export { FaCar as VehiclesIcon } from "react-icons/fa";            
-export { FaUserCircle as ProfileIcon } from "react-icons/fa";      
